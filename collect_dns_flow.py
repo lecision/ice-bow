289bf2d7269021197f1533b3e5bc4d371144e875
lecision <lecision@163.com>
2019-12-06 18:13:13 -0800

    pyshark dns_layer

--- collect_dns_flow.py
+++ collect_dns_flow.py
@@ -4,19 +4,21 @@ import os
 import subprocess
 import time
 
-pcapfile = '/home/lecision/workspace/stream/ISCX_Botnet-Testing.pcap'
+pcapfile = '/home/liyucheng/workspace/stream/DNS.pcapng'
 def collect_dns_stream():
     cap = pyshark.FileCapture(pcapfile, display_filter='dns')
 
-    i = 0
-
+    print dir(cap[0].dns)
+'''
+    i = 0dir(
+    qry_domain_name = []
     for pkt in cap:
         if pkt.dns.qry_name:
             print ('DNS Request from %s: %s ' % (pkt.ip.src, pkt.dns.qry_name))
         elif pkt.dns.resp_name:
             print ('DNS Response from %s: %s ' % (pkt.ip.src, pkt.dns.resp_name))
         i += 1
-
+'''
 if __name__ == '__main__':
     collect_dns_stream()
 
